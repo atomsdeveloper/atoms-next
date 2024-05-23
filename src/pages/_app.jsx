ad220dcd16d31be03f42c8de9786a4fadcd3bfa6
renanns <atomsfrontenf@gmail.com>
2024-05-23 14:11:29 -0300

    fix: correcting error in prettier in component

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,18 +1,18 @@
-import P from "prop-types";
+import P from 'prop-types';
 
-import { theme } from "../styles/theme";
-import { ThemeProvider } from "styled-components";
+import { theme } from '../styles/theme';
+import { ThemeProvider } from 'styled-components';
 
 function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <Component {...pageProps} />;
     </ThemeProvider>
-  )
+  );
 }
 export default App;
 
 App.propTypes = {
   Component: P.elementType.isRequired,
   pageProps: P.any,
-}
+};
